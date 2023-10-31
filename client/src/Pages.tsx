f6ee914cffb6f705bea531314c19716ce531f761
Manish Chhetri <65161301+chhetri123@users.noreply.github.com>
2023-10-31 13:23:17 +0000

    creating poll

--- client/src/Pages.tsx
+++ client/src/Pages.tsx
@@ -5,11 +5,13 @@ import Create from './pages/Create';
 import Join from './pages/Join';
 import Welcome from './pages/Welcome';
 import { AppPage, state } from './state';
+import { WaitingRoom } from './pages/WaitingRoom';
 
 const routeConfig = {
   [AppPage.Welcome]: Welcome,
   [AppPage.Create]: Create,
   [AppPage.Join]: Join,
+  [AppPage.WaitingRoom]: WaitingRoom,
 };
 
 const Pages: React.FC = () => {
